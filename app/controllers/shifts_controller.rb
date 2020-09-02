98f36157ccc661e15f498adee6775b5585ff2948
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2020-09-02 14:00:00 +0900

    必要なメソッドを追加する。

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -1,4 +1,26 @@
 class ShiftsController < ApplicationController
   def index
+    @shift = Shift.all
   end
+
+  def new
+    @shift = Shift.new
+  end
+
+  def edit; end
+
+  def create
+    @shift = Shift.new(shift_params)
+  end
+
+  def udpate; end
+
+  def destroy; end
+
+  private
+
+  def shift_params
+    params.require(:shift).permit(:name, :day)
+  end
+
 end
